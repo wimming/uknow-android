9271b0bb82c33e1fa3ddf720c11604390ac6a72b
yongningfu <535802703@qq.com>
2016-11-12 04:45:54 +0800

    更新找人页面

--- app/src/main/java/com/xuewen/xuewen/AboutMeFragmentTwo.java
+++ app/src/main/java/com/xuewen/xuewen/AboutMeFragmentTwo.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.xuewen.adapter.AboutMeQuestionListAskAdapter;
 import com.xuewen.bean.Question;
 
 import java.util.ArrayList;
@@ -25,25 +26,26 @@ public class AboutMeFragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_about_me_fragment_two, container, false);
-//        ListView questionListView = (ListView) rootView.findViewById(R.id.aboutMe_unanswer_list);
-//        List<Question> questionList = new ArrayList<>();
-//
-//        Question q;
-//        for (int i = 0; i < 10; ++i) {
-//            q = new Question("师兄好，软件学院的学生毕业后有哪些出路呢？");
-//            q.ans_description = "张三的回答";
-//            questionList.add(q);
-//        }
-//        AboutMeQuestionListUnAnswerAdapter questionListAdapter = new AboutMeQuestionListUnAnswerAdapter(questionList, getActivity());
-//
-//        questionListView.setAdapter(questionListAdapter);
-//        questionListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(getActivity(), QuestionDetailActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        ListView questionListView = (ListView) rootView.findViewById(R.id.aboutMe_ask_list);
+        List<Question> questionList = new ArrayList<>();
+
+        Question q;
+        for (int i = 0; i < 10; ++i) {
+            q = new Question();
+            q.que_description = "师兄好，软件学院的学生毕业后有哪些出路呢？";
+            q.que_username = "张三的回答";
+            questionList.add(q);
+        }
+        AboutMeQuestionListAskAdapter questionListAdapter = new AboutMeQuestionListAskAdapter(questionList, getActivity());
+
+        questionListView.setAdapter(questionListAdapter);
+        questionListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), QuestionDetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
