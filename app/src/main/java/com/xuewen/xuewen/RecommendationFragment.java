416b5715ffd459fd78558c3a4bc379b97cddb885
wimming <623813445@qq.com>
2016-11-22 10:35:55 +0800

    实现网络访问与界面的绑定

--- app/src/main/java/com/xuewen/xuewen/RecommendationFragment.java
+++ app/src/main/java/com/xuewen/xuewen/RecommendationFragment.java
@@ -1,5 +1,6 @@
 package com.xuewen.xuewen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.xuewen.adapter.QRListAdapter;
+import com.xuewen.bean.QRBean;
 import com.xuewen.bean.Question;
 import com.xuewen.networkservice.ApiService;
 import com.xuewen.networkservice.QQidResult;
@@ -34,72 +37,61 @@ public class RecommendationFragment extends Fragment {
     {
         View rootView = inflater.inflate(R.layout.fragment_0, container, false);
 
-        ListView questionListView = (ListView)rootView.findViewById(R.id.question_list_view);
-        List<Question> questionList = new ArrayList<>();
+//        ListView questionListView = (ListView)rootView.findViewById(R.id.question_list_view);
+//        List<Question> questionList = new ArrayList<>();
+//
+//        Question q;
+//        for (int i = 0; i < 10; ++i) {
+//            q = new Question("师兄好，软件学院的学生毕业后有哪些出路呢？");
+//            q.ans_description = "张三 | 清华大学计算机系，ACM校队队长，喜欢钻研算法，喜欢钻研算法";
+//            q.heard = 100;
+//            q.liked = 10;
+//            q.ans_headimgurl = "http://www.jd.com/favicon.ico";
+//            questionList.add(q);
+//        }
+//
+//        QuestionListAdapter questionListAdapter = new QuestionListAdapter(questionList, getActivity());
+//
+//        questionListView.setAdapter(questionListAdapter);
+//        questionListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+////                Intent intent = new Intent(getActivity(), QuestionDetailActivity.class);
+////                startActivity(intent);
+//
+//            }
+//        });
 
-        Question q;
-        for (int i = 0; i < 10; ++i) {
-            q = new Question("师兄好，软件学院的学生毕业后有哪些出路呢？");
-            q.ans_description = "张三 | 清华大学计算机系，ACM校队队长，喜欢钻研算法，喜欢钻研算法";
-            q.heard = 100;
-            q.liked = 10;
-            q.ans_headimgurl = "http://www.jd.com/favicon.ico";
-            questionList.add(q);
-        }
-
-        QuestionListAdapter questionListAdapter = new QuestionListAdapter(questionList, getActivity());
-
-        questionListView.setAdapter(questionListAdapter);
-        questionListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final ListView listView = (ListView)rootView.findViewById(R.id.listView);
+        final List<QRBean> list = new ArrayList<>();
+        final QRListAdapter adapter = new QRListAdapter(list, getActivity());
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(getActivity(), QuestionDetailActivity.class);
-//                startActivity(intent);
+                Intent intent = new Intent(getActivity(), QuestionDetailActivity.class);
+                startActivity(intent);
 
-                ApiService apiService = ApiService.retrofit.create(ApiService.class);
-                final Call<QRResult> call =
-                        apiService.requestQR();
+            }
+        });
 
-                call.enqueue(new Callback<QRResult>() {
-                    @Override
-                    public void onResponse(Call<QRResult> call, Response<QRResult> response) {
-                        Log.e("MyRequest when success ", response.body().data.get(1).listeningNum+"");
-                        Toast.makeText(getActivity(), "MyRequest when success "+response.body().data.get(1).listeningNum+"", Toast.LENGTH_LONG).show();
-//
-//                        String str = "{\n" +
-//                                "  'status': 200,\n" +
-//                                "  'result': [ \n" +
-//                                "    {\n" +
-//                                "      'que__id': 1,\n" +
-//                                "      'que__description': '问题描述',\n" +
-//                                "      'ans_id': 11,\n" +
-//                                "      'ans_status': '回答者身份',\n" +
-//                                "      'ans_description': '回答者描述',\n" +
-//                                "      'ans_headimgurl': '回答者头像路径',\n" +
-//                                "      'heard': 12,\n" +
-//                                "      'liked': 14\n" +
-//                                "    },\n" +
-//                                "    {\n" +
-//                                "      'que__id': 1,\n" +
-//                                "      'que__description': '问题描述',\n" +
-//                                "      'ans_id': 11,\n" +
-//                                "      'ans_status': '回答者身份',\n" +
-//                                "      'ans_description': '回答者描述',\n" +
-//                                "      'ans_headimgurl': '回答者头像路径',\n" +
-//                                "      'heard': 12,\n" +
-//                                "      'liked': 14\n" +
-//                                "    }\n" +
-//                                "  ]\n" +
-//                                "}";
-//                        Gson gson = new Gson();
-//                        NetworkResult networkResult = gson.fromJson(str, NetworkResult.class);
-                    }
-                    @Override
-                    public void onFailure(Call<QRResult> call, Throwable t) {
-                        Log.e("MyRequest when failure", "Something went wrong: " + t.getMessage());
-                        Toast.makeText(getActivity(), "MyRequest when success "+t.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
+        ApiService apiService = ApiService.retrofit.create(ApiService.class);
+        Call<QRResult> call =
+                apiService.requestQR();
+
+        call.enqueue(new Callback<QRResult>() {
+            @Override
+            public void onResponse(Call<QRResult> call, Response<QRResult> response) {
+                Toast.makeText(getActivity(), "request success, list size : "+response.body().data.size()+"", Toast.LENGTH_LONG).show();
+                list.clear();
+                for (QRBean item : response.body().data) {
+                    list.add(item);
+                }
+                adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onFailure(Call<QRResult> call, Throwable t) {
+                Toast.makeText(getActivity(), "request failure : "+t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
 
