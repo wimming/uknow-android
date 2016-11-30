b0f021123d8c6f356ca8b10b08c6d0b23b8076ef
yongningfu <535802703@qq.com>
2016-12-01 02:18:21 +0800

    前端页面基本完成

--- app/src/main/java/com/xuewen/xuewen/SettingActivity.java
+++ app/src/main/java/com/xuewen/xuewen/SettingActivity.java
@@ -4,19 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 
 public class SettingActivity extends AppCompatActivity {
 
-    @BindView(R.id.listView) ListView listView;
+//    @BindView(R.id.listView) ListView listView;
+
+    private static  final String[] textData = {"使用帮助","关于学问", "意见反馈", "版本更新"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        List<Map<String, String>> list_data = new ArrayList<Map<String, String>>();
+        for (int i = 0; i < textData.length; i++) {
+            Map<String, String> tempMap = new HashMap<String, String>();
+            tempMap.put("textview", textData[i]);
+            list_data.add(tempMap);
+        }
 
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, list_data,
+                R.layout.activity_setting_item, new String[] {"textview"}, new int[] {R.id.textview});
 
+        listView.setAdapter(simpleAdapter);
     }
 }
