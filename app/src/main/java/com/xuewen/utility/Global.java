91fe449f95300668eec951458d43ae320d971aec
HuangYuming <623813445@qq.com>
2017-04-07 12:58:40 +0800

    提交前 static stroage 时间数据 reverse

--- app/src/main/java/com/xuewen/utility/Global.java
+++ app/src/main/java/com/xuewen/utility/Global.java
@@ -23,7 +23,7 @@ public class Global {
 
     public DisplayImageOptions circleBitmapOptions;
     public String baseUrl = "http://123.207.233.226:5000/";
-//    public String baseUrl = "http://172.22.192.65:5000/";
+//    public String baseUrl = "http://172.19.101.43:5000/";
 //    public String baseUrl = "http://192.168.1.106:5000/";
 
     public String baseAvatarUrl = baseUrl+"static/avatar/";
